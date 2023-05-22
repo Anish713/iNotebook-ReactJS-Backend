08c9e727cf0a77bfd760c4502c1c53591190242c
“Anish713” <shresthaanish713@gmail.com>
2023-05-22 18:29:50 +0545

    First code refactored

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
-const connectToMongo = require("./db");
-const express = require("express");
-connectToMongo();
+const connectToMongo = require('./db');
+const express = require('express')
 
-const app = express();
-const port = 5000;
+connectToMongo();
+const app = express()
+const port = 5000
 
 app.use(express.json())
 
-//Routes
+// Available Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
 
-app.use("/api/auth", require("./routes/auth"));
-app.use("/api/notes", require("./routes/notes"));
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`);
-});
+  console.log(`Example app listening at http://localhost:${port}`)
+})
